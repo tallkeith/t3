db90825afdf8c3f671728485dc61ea215c6437e6
tallkeith <tallestkeith@gmail.com>
2016-06-14 12:21:14 -0400

    Added instructions and about

--- t3.rb
+++ t3.rb
@@ -2,11 +2,74 @@ require "pry"
 
 board = [8,1,6,3,5,7,4,9,2]
 
-def greeting
+def greeting(board)
   puts
-  puts "Welcome to Magic Square Tic Tac Toe!"
-  puts "Try to get three numbers that equal 15 to win..."
-  puts
+	puts "Welcome to Magic Square Tic Tac Toe."
+	sleep 1.5
+	puts 
+	puts "Please choose an option below:"
+	puts "======================================================="
+	puts "| 1. Play                                             |"
+	puts "|     Try to get 3 numbers that add up to 15.         |"
+	puts "| 2. Instructions                                     |"
+	puts "|     Learn how to play                               |"
+	puts "| 3. About                                            |"
+	puts "|     Some info about the author of this code         |"
+	puts "| 4. Exit                                             |"
+	puts "|     Leave the game                                  |"
+	puts "======================================================="
+	puts
+
+	selection = gets.chomp.to_i
+
+	until (1..5).include?(selection)
+		puts "Please select an option 1 - 3."
+		selection = gets.chomp.to_i
+	end
+	
+	case selection 
+		when 1 
+			play_game(board)
+		when 2
+			instructions(board)
+		when 3
+			about(board)
+		when 4
+			exit
+		else
+			puts "Please select an option 1 - 3."
+	end
+end
+
+def instructions(board)
+	puts
+	puts "X always goes first."
+	puts
+	puts "Players alternate placing Xs and Os on the board until either "
+	puts "(a) one player has three in a row, horizontally, vertically or diagonally; or "
+	puts "(b) all nine squares are filled."
+	puts
+	puts "If a player is able to draw three Xs or three Os in a row, that player wins."
+	puts
+	puts "If all nine squares are filled and neither player has three in a row, "
+	puts "the game is a draw."
+	
+	greeting(board)
+end
+
+def about(board)
+	puts
+	puts "Game created by Keith Nash. You can see more of my work at Tallkeith.com, "
+	puts "or checkout my github repo at github.com/tallkeith"
+	puts
+	puts "I am tall. Like, REALLY tall. I am between 6'6 and 6'9, "
+	puts "depending on which convenience store I am walking out of."
+	puts
+	puts "I like Bacon, Star Wars, and Cake. I will go to great lengths for cake. "
+	puts "I attend weddings of people I do not like, because I know they will give me cake."
+	puts
+
+	greeting(board)
 end
 
 def show_board(board)
@@ -70,7 +133,7 @@ end
 
 def play_again?
 	board = [8,1,6,3,5,7,4,9,2]
-	puts "Would you like to play again?"
+	puts "Would you like to play again? y/n"
 	choice = gets.chomp.downcase
 	until ["y", "n"].include?(choice)
 		puts "Please choose 'Y' or 'N'"
@@ -84,7 +147,7 @@ def play_again?
 end
 
 def play_game(board)
-	greeting
+	
 	player_array = []
 	player = "X"
 	comp_array = []
@@ -94,6 +157,7 @@ def play_game(board)
 		show_board(board)
 		if current_player == player
 			puts "Human Player's turn"
+			puts "Your numbers are: #{player_array}"
 			choice = player_move(player_array, comp_array, board)
 			player_array.push(choice)
 		  spot = board.index(choice)
@@ -112,5 +176,4 @@ def play_game(board)
 	finish_game(current_player, comp, player, board, player_array)
 end
 
-play_game(board)
-
+greeting(board)
